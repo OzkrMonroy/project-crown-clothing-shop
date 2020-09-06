ae6a1f6d27113aeefd0f7fc0b80389b54f29f111
Oscar Monroy <oscarmonroy657@gmail.com>
2020-09-06 08:05:33 -0600

    Created project with Create React App and added files for Homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import HomePage from "./homepage.component";
+
+// bigelow rules (fuente)
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;

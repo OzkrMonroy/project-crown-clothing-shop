ddb8fd18f44302b2778d96738e4b252318c001c8
Oscar Monroy <oscarmonroy657@gmail.com>
2020-09-10 05:57:56 -0600

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./homepage.component";
+import { Switch, Route } from "react-router-dom";
 
 // bigelow rules (fuente)
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+      </Switch>
     </div>
   );
 }

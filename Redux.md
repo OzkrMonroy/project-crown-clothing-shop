b5305b93528a6885d637418784c2c041ecfd1ec9
Oscar Monroy <oscarmonroy657@gmail.com>
2021-01-19 20:32:55 -0600

    Moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- Redux.md
+++ Redux.md
@@ -3,3 +3,6 @@
 Para empezar, el middleware que vamos a usar es *logger*. Por lo tanto vamos a instalar las siguientes dependencias:
 
 npm i --save redux redux-logger react-redux
+
+
+Al manejo de datos a través de objetos en lugar de arreglos se le llama **Data Normalization**
---
+++ src/redux/shop/shop.selector.js
@@ -0,0 +1,19 @@
+import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
+
+const selectShop = state => state.shop
+
+export const selectShopItems = createSelector(
+  [selectShop],
+  shop => shop.shopItems
+)
+
+export const selectCollectionsForPreview = createSelector(
+  [selectShopItems],
+  collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = memoize(collectionUrlParam => createSelector(
+  [selectShopItems],
+  collections => collections[collectionUrlParam]
+))
